cc79ed6a254410f433937793058a19b96670e83e
Matheus K. Bastiani <matheusbastiani08@gmail.com>
2026-04-09 12:24:38 -0300

    07/04/2026 20:51

--- exercicios/041.py
+++ exercicios/041.py
@@ -8,3 +8,43 @@ frase que diga se o número é:
     positivo ou negativo;
     inteiro ou decimal.
 """
+import math
+
+def positivo_inteiro(entrada):
+
+    if entrada >= 0: print('Numero Positivo')
+    else: print('Numero Negativo')
+
+    if math.floor(entrada) == entrada: print('Numero Inteiro')
+    else: print('Numero Decimal')
+
+"Inicio do Codigo"
+
+n1 = float(input('Insira o primeiro numero: '))
+n2 = float(input('Insira o segundo numero: '))
+
+instrucao = str(input('Insira uma operacao basica a fazer, soma; subtracao; divisao; multiplicacao: '))
+
+if instrucao == "soma":
+    result = n1 + n2
+    print(result)
+    positivo_inteiro(result)
+
+elif instrucao == "subtracao":
+    result = n1 - n2
+    print(result)
+    positivo_inteiro(result)
+
+elif instrucao == "multiplicacao":
+    result = n1 * n2
+    print(result)
+    positivo_inteiro(result)
+
+elif instrucao == "divisao":
+    result = n1 / n2
+    print(result)
+    positivo_inteiro(result)
+
+else:
+    print('Entrada invalida')
+    exit()
